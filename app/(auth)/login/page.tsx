6049fc7fedc5a867cde5727c9cb6f17596a05898
meisterjustice <eziefulejustice@gmail.com>
2025-05-18 10:54:24 +0100

    refactor: simplify social login component and enhance functionality
    - Removed unused imports and commented-out code for cleaner structure.
    - Integrated `useConnectToFarcaster` for handling Farcaster login.
    - Updated login handling to improve user experience with conditional login logic.
    - Streamlined the dialogue component for social connection with updated props.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,32 +1,27 @@
 "use client";
 
-import { useState, useEffect, JSX } from "react";
-import Image from "next/image";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { usePrivy } from "@privy-io/react-auth";
 
-import {
-  KPButton,
-  KPDialougue,
-  KPFullscreenLoader,
-  KPLoader,
-} from "@/components";
-import { CheckIcon } from "@/public/icons";
+import { KPButton, KPDialougue, KPFullscreenLoader } from "@/components";
 import useSystemFunctions from "@/hooks/useSystemFunctions";
-import usePrivyLinkedAccounts from "@/hooks/usePrivyLinkedAccounts";
-
-const friends = [
-  { name: "Kripson", image: "/images/kripson.jpeg" },
-  { name: "Kripson", image: "/images/kripson.jpeg" },
-  { name: "Kripson", image: "/images/kripson.jpeg" },
-];
+import useConnectToFarcaster from "@/hooks/useConnectToFarcaster";
 
 const Social = () => {
   const { user, ready, login } = usePrivy();
-  const { linkedFarcaster, linkedTwitter } = usePrivyLinkedAccounts();
+  const { loginToFarcasterFrame, isFrameLoaded } = useConnectToFarcaster();
   const { navigate } = useSystemFunctions();
 
   const [stage, setStage] = useState<"connect" | "setup">("connect");
+
+  const handleLogin = async () => {
+    if (isFrameLoaded) {
+      return loginToFarcasterFrame();
+    }
+
+    return login();
+  };
 
   useEffect(() => {
     if (stage === "setup") {
@@ -44,87 +39,6 @@ const Social = () => {
     }
   }, [ready, user]);
 
-  const dialogues: Record<string, JSX.Element> = {
-    connect: (
-      <KPDialougue
-        title="connect with your social"
-        subtitle="Play with your frens. Share your wins. See who’s already in the fleet."
-        showCloseButton
-        onClose={() => {}}
-        showKripsonImage
-        primaryCta={{
-          title: "Connect",
-          onClick: login,
-          variant: "primary",
-          isMachine: true,
-          fullWidth: true,
-          multipleicons: ["x", "farcaster"],
-        }}
-      >
-        <div className="w-full flex flex-col items-stretch mt-5 gap-1"></div>
-      </KPDialougue>
-    ),
-    // connected: (
-    //   <KPDialougue
-    //     title="connect with your social"
-    //     subtitle="Play with your frens. Share your wins. See who’s already in the fleet."
-    //     showCloseButton
-    //     onClose={() => {}}
-    //     ctaText="Connected"
-    //   >
-    //     <div className="flex flex-col items-center justify-center self-stretch w-full gap-5 text-center">
-    //       <div className="size-[84px] max-sm:size-[56.14px] rounded-lg bg-white flex items-center justify-center relative">
-    //         {linkedFarcaster && (
-    //           <Image
-    //             src="/images/farcaster.png"
-    //             alt="farcaster"
-    //             width={64}
-    //             height={64}
-    //             quality={100}
-    //             className="size-16 max-sm:size-[42.78px] object-cover rounded-md"
-    //           />
-    //         )}
-    //
-    //         {linkedTwitter && (
-    //           <Image
-    //             src="/images/x.png"
-    //             alt="twitter"
-    //             width={64}
-    //             height={64}
-    //             quality={100}
-    //             className="size-16 max-sm:size-[42.78px] object-cover rounded-md"
-    //           />
-    //         )}
-    //         <div className="absolute right-1 bottom-4 max-sm:bottom-2">
-    //           <CheckIcon className="max-sm:size-[17.38px]" />
-    //         </div>
-    //       </div>
-    //
-    //       <div className="flex items-center justify-center gap-2">
-    //         <div className="flex items-center justify-center -space-x-2">
-    //           {friends.map((friend, index) => (
-    //             <Image
-    //               key={index}
-    //               src={friend.image}
-    //               alt={friend.name}
-    //               width={32}
-    //               height={32}
-    //               className="size-[32px] max-sm:size-[21.39px] rounded-full border-2 max-sm:border border-white"
-    //               quality={100}
-    //               style={{ zIndex: friends.length - index }}
-    //             />
-    //           ))}
-    //         </div>
-    //
-    //         <span className="text-[16px] leading-[100%] max-sm:text-[10.69px] text-primary-50">
-    //           {friends.length} friends already playing!
-    //         </span>
-    //       </div>
-    //     </div>
-    //   </KPDialougue>
-    // ),
-  };
-
   return (
     <div className="w-full h-full flex items-center justify-center relative">
       <AnimatePresence mode="wait">
@@ -136,7 +50,23 @@ const Social = () => {
             exit={{ scale: 0.9, opacity: 0 }}
             transition={{ duration: 0.4 }}
           >
-            {dialogues[stage]}
+            <KPDialougue
+              title="connect with your social"
+              subtitle="Play with your frens. Share your wins. See who’s already in the fleet."
+              showCloseButton
+              onClose={() => {}}
+              showKripsonImage
+              primaryCta={{
+                title: "Connect",
+                onClick: handleLogin,
+                variant: "primary",
+                isMachine: true,
+                fullWidth: true,
+                multipleicons: ["x", "farcaster"],
+              }}
+            >
+              <div className="w-full flex flex-col items-stretch mt-5 gap-1"></div>
+            </KPDialougue>
           </motion.div>
         )}
       </AnimatePresence>
