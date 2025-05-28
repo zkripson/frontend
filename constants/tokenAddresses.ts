4574a66a4be6e0e622fa5247ae5e4b94c4948f89
meisterjustice <eziefulejustice@gmail.com>
2025-05-28 22:11:26 +0100

    trigger deploy

--- constants/tokenAddresses.ts
+++ constants/tokenAddresses.ts
@@ -1,5 +1,4 @@
 import { isDevEnv } from "@/providers/PrivyProvider";
-
 const usdcBaseSepolia = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
 const usdcBase = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
 const shipBaseSepolia = "0x8853F0c059C27527d33D02378E5E4F6d5afB574a";
