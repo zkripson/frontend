8efe1f3ddb72cb2eaf4c74052160de883e3b24f5
meisterjustice <eziefulejustice@gmail.com>
2025-05-29 00:12:24 +0100

    fix: update Content Security Policy in next.config.ts
    - Added https://api.bship.fun to the allowed sources for script execution in the Content Security Policy.
    - Removed trailing semicolon from the previous entry for consistency.

--- next.config.ts
+++ next.config.ts
@@ -50,7 +50,8 @@ const ContentSecurityPolicy = `
     https://sepolia.base.org
     https://api.pimlico.io/v2/84532/rpc
     https://auth.privy.io
-    https://auth.privy.io/api/v2/farcaster/init;
+    https://auth.privy.io/api/v2/farcaster/init
+    https://api.bship.fun;
 
   
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com;
