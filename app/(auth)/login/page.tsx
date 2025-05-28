f5627d16811129cfd7f56e884932bee5e858124a
meisterjustice <eziefulejustice@gmail.com>
2025-05-28 22:44:08 +0100

    feat: integrate profile creation on wallet connection in RootApp
    - Added createProfile functionality in RootApp to automatically create a user profile when a wallet is connected and linked accounts are available.
    - Removed redundant profile creation logic from the Social component in the login page for cleaner code.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,21 +2,16 @@
 
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { LoginModalOptions, usePrivy } from "@privy-io/react-auth";
+import { usePrivy } from "@privy-io/react-auth";
 
 import { KPDialougue, KPFullscreenLoader } from "@/components";
 import useSystemFunctions from "@/hooks/useSystemFunctions";
 import useConnectToFarcaster from "@/hooks/useConnectToFarcaster";
-import { usePlayerActions } from "@/store/player/actions";
-import usePrivyLinkedAccounts from "@/hooks/usePrivyLinkedAccounts";
 
 const Social = () => {
   const { user, ready, login } = usePrivy();
   const { loginToFarcasterFrame, isFrameLoaded } = useConnectToFarcaster();
   const { navigate } = useSystemFunctions();
-  const { createProfile } = usePlayerActions();
-  const { linkedFarcaster, linkedTwitter, activeWallet } =
-    usePrivyLinkedAccounts();
 
   const [stage, setStage] = useState<"connect" | "setup">("connect");
   const [loading, setLoading] = useState(false);
@@ -54,29 +49,6 @@ const Social = () => {
       setStage("setup");
     }
   }, [ready, user]);
-
-  useEffect(() => {
-    if (activeWallet && (linkedFarcaster || linkedTwitter)) {
-      const avatar =
-        linkedFarcaster?.pfp || linkedTwitter?.profilePictureUrl || "";
-      const username =
-        linkedFarcaster?.username || linkedTwitter?.username || "";
-
-      createProfile({
-        address: activeWallet.address,
-        avatar,
-        username,
-        preferences: {
-          animationsEnabled: true,
-          autoSubmitOnHit: true,
-          notifications: true,
-          soundEnabled: true,
-          theme: "dark",
-        },
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeWallet, linkedFarcaster, linkedTwitter]);
 
   return (
     <div className="w-full h-full flex items-center justify-center relative">
