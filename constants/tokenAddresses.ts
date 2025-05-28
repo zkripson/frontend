61deaa384584ddf35c43c818adc017c4591cdf14
meisterjustice <eziefulejustice@gmail.com>
2025-05-28 22:09:59 +0100

    fix: Update bettingBase address in tokenAddresses.ts
    - Changed the bettingBase address to the correct value for improved functionality.

--- constants/tokenAddresses.ts
+++ constants/tokenAddresses.ts
@@ -5,7 +5,7 @@ const usdcBase = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
 const shipBaseSepolia = "0x8853F0c059C27527d33D02378E5E4F6d5afB574a";
 const shipBase = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02912";
 const bettingBaseSepolia = "0x31985259fECd3C35f969FB3AfAcb3f98D2daeef8";
-const bettingBase = "0x31985259fECd3C35f969FB3AfAcb3f98D2daeef8";
+const bettingBase = "0x2b859551fBC83219abf2C4965333013DC972D228";
 
 const TOKEN_ADDRESSES = {
   USDC: isDevEnv ? usdcBaseSepolia : usdcBase,
