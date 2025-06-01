c6abb3a0b6777ab65c025decebd730daff387352
meisterjustice <eziefulejustice@gmail.com>
2025-06-01 14:19:10 +0100

    fix: ensure default amount in approveTransfer for StakeScreen
    - Updated StakeScreen to default to an amount of 1 if no value is provided when calling approveTransfer.
    - This change enhances the user experience by preventing potential errors related to undefined amounts during the approval process.

--- app/indexComponents/StakeScreen.tsx
+++ app/indexComponents/StakeScreen.tsx
@@ -78,7 +78,7 @@ const StakeScreen: React.FC<StakeScreenProps> = ({ onBack, nextScreen }) => {
         );
       }
       setApprovingTransfer(true);
-      await approveTransfer(Number(amount));
+      await approveTransfer(Number(amount || 1));
 
       setTimeout(() => {
         nextScreen();
